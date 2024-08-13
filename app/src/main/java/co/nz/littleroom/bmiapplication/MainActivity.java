60400bbeb13f1f253d9d46e538e0d6d7dd85def3
James Wilson <jmboby@gmail.com>
2024-08-14 07:34:07 +1200

    Added app wireframe, image and Toast alert

--- app/src/main/java/co/nz/littleroom/bmiapplication/MainActivity.java
+++ app/src/main/java/co/nz/littleroom/bmiapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package co.nz.littleroom.bmiapplication;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toast.makeText(this, "A wee toast alert!", Toast.LENGTH_LONG).show();
     }
 }
