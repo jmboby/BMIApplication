2e78ab8146fffc065ae7e05797be595a56d2faec
James Wilson <jmboby@gmail.com>
2024-08-14 20:29:06 +1200

    Added id's to activity_main.xml and referenced in MainActivity.java

--- app/src/main/java/co/nz/littleroom/bmiapplication/MainActivity.java
+++ app/src/main/java/co/nz/littleroom/bmiapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package co.nz.littleroom.bmiapplication;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,11 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Toast.makeText(this, "A wee toast alert!", Toast.LENGTH_LONG).show();
+
+        findViewById(R.id.text_view_result);
+
+        RadioButton maleRadioButton = findViewById(R.id.radio_button_male);
+        RadioButton femaleRadioButton = findViewById(R.id.radio_button_female);
+        TextView ageTextView = findViewById(R.id.edit_text_age);
+        TextView feetTextView = findViewById(R.id.edit_text_feet);
+        TextView inchesTextView = findViewById(R.id.edit_text_inches);
+        TextView weightTextView = findViewById(R.id.edit_text_weight);
+        Button calculateButton = findViewById(R.id.button_calculate);
+        TextView resultText = findViewById(R.id.text_view_result);
+
+
     }
 }
